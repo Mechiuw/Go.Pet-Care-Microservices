c2688b8b6b1a720dfbe7b4ee10ee6f48cf4b3f39
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-08 11:20:42 +0700

    [FEATURE] Added Helper Func and Client Update Service

---
+++ src/helper/clientscanner.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	"database/sql"
+	"ginpet/src/model"
+)
+
+func scanClient(rows *sql.Rows) []model.Client {
+	clients := []model.Client{}
+	var err error
+
+	for rows.Next() {
+		client := model.Client{}
+		err = rows.Scan(
+			&client.Id,
+			&client.Name,
+			&client.ProfileNumber,
+			&client.Address,
+			&client.PhoneNumber,
+			&client.Email,
+		)
+		if err != nil {
+			panic("failed to scan row: " + err.Error())
+		}
+
+		clients = append(clients, client)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic("error occurred during row iteration: " + err.Error())
+	}
+	return clients
+}
--- src/service/clientservice.go
+++ src/service/clientservice.go
@@ -9,7 +9,7 @@ import (
 var connection = db.Pool()
 
 func CREATE_CLIENT(client model.Client) (model.Client, error) {
-	sqlStatement := `INSERT INTO client (id,name,profilenumber,address,phonenumber,email) VALUES ($1,$2,$3,$4,$5,$6)`
+	sqlStatement := `INSERT INTO client (id,name,profilenumber,address,phonenumber,email) VALUES ($1,$2,$3,$4,$5,$6);`
 
 	_, err :=
 		connection.Exec(
@@ -24,6 +24,33 @@ func CREATE_CLIENT(client model.Client) (model.Client, error) {
 		return model.Client{}, fmt.Errorf("failed to create client: %w", err)
 	}
 
-	fmt.Println("successfully added data!")
+	fmt.Println("successfully added data [201]")
 	return client, nil
+}
+
+func UPDATE_CLIENT(id string, updateClient map[string]string) (model.Client, error) {
+	sqlStatement := `UPDATE client SET $1=$2 WHERE id = $3;`
+	newPatchClient := model.Client{}
+	var col string
+	var val string
+	for k, v := range updateClient {
+		col = k
+		val = v
+	}
+
+	_, err := connection.Exec(sqlStatement, col, val, id)
+	if err != nil {
+		return model.Client{}, fmt.Errorf("failed to update client: %w", err)
+	}
+
+	getSqlStatement := `SELECT * FROM client where id =$1`
+	err = connection.QueryRow(getSqlStatement, id).Scan(
+		&newPatchClient.Id, &newPatchClient.Name, &newPatchClient.ProfileNumber, &newPatchClient.Address, &newPatchClient.PhoneNumber, &newPatchClient.Email,
+	)
+	if err != nil {
+		return newPatchClient, fmt.Errorf("failed to fetch client response: %w", err)
+	}
+
+	fmt.Println("successfully updated data [200]")
+	return newPatchClient, nil
 }
