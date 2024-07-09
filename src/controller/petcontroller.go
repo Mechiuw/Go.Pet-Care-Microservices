690653e6550d425bfa530f6c05d4508e84446734
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-09 17:09:57 +0700

    [FEATURE] Added Update pet to Pet controller

--- src/controller/petcontroller.go
+++ src/controller/petcontroller.go
@@ -23,7 +23,30 @@ func CreatePet(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
+		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"data": createdPet, "message": "successfully created pet"})
+	c.JSON(http.StatusCreated, gin.H{"message": "successfully created pet", "data": createdPet})
+}
+
+func UpdatePet(c *gin.Context) {
+	id := c.Param("id")
+	var pet model.Pet
+
+	if err := c.BindJSON(&pet); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	updatePet, err := service.UPDATE_PET(id, pet)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "successfully update pet", "data": updatePet})
 }
