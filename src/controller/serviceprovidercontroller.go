5dc5566c401fb4d2dcba3402ce504945168d64bd
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-10 00:52:11 +0700

    [FEATURE] Added CreateSp to controller

--- src/controller/serviceprovidercontroller.go
+++ src/controller/serviceprovidercontroller.go
@@ -1,8 +1,27 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginpet/src/model"
+	"net/http"
 
-func CreateSp(c *gin.Context)  {}
+	"github.com/gin-gonic/gin"
+)
+
+func CreateSp(c *gin.Context) {
+	var sp model.ServiceProvider
+
+	if err := c.BindJSON(&sp); err != nil {
+		c.JSON(http.StatusCreated, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "successfully created service provider",
+		"data":    sp,
+	})
+}
 func UpdateSp(c *gin.Context)  {}
 func DeleteSp(c *gin.Context)  {}
 func GetAllSp(c *gin.Context)  {}
