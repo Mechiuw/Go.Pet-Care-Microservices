2f35284f770157b914d28693c29b97b5cf4b695c
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-10 12:16:45 +0700

    [FEATURE] Added Create Service wip on other func

--- src/service/appointmentservice.go
+++ src/service/appointmentservice.go
@@ -10,21 +10,27 @@ import (
 var appointment_connection = db.Pool()
 var validator = helper.NewValidator()
 
-func CREATE_APPOINTMENT(app model.Appointment) (model.Appointment,error){
+func CREATE_APPOINTMENT(app model.Appointment) (model.Appointment, error) {
 	sqlStatement := `INSERT INTO appointment (Id,petid,serviceproviderid,servicetype,servicetime) VALUES ($1,$2,$3,$4,$5)`
 
-	validated := 
+	err := validator.ValidateAppointment(app)
 
-	_,err := appointment_connection.Exec(sqlStatement,)
+	if err != nil {
+		return model.Appointment{}, fmt.Errorf("validation error: %w", err)
+	}
+	_, err = appointment_connection.Exec(sqlStatement, app.Id, app.PetId, app.ServiceProviderId, app.ServiceType, app.ServiceTime)
+	if err != nil {
+		return model.Appointment{}, fmt.Errorf("failed to create appointment: %w", err)
+	}
 
 	appResponse := model.Appointment{
-		Id: app.Id,
-		PetId: app.PetId,
+		Id:                app.Id,
+		PetId:             app.PetId,
 		ServiceProviderId: app.ServiceProviderId,
-		ServiceType: app.ServiceType,
-		ServiceTime: app.ServiceTime,
+		ServiceType:       app.ServiceType,
+		ServiceTime:       app.ServiceTime,
 	}
 
 	fmt.Println("successfully create appointment")
-	return appResponse,nil
+	return appResponse, nil
 }
