a1b01ddee90e1b5d8f7c4af812224a0c89e1ffff
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-10 12:24:53 +0700

    [FEATURE] Added Update appointment Service

--- src/service/appointmentservice.go
+++ src/service/appointmentservice.go
@@ -34,3 +34,29 @@ func CREATE_APPOINTMENT(app model.Appointment) (model.Appointment, error) {
 	fmt.Println("successfully create appointment")
 	return appResponse, nil
 }
+
+func UPDATE_APPOINTMENT(id string, app model.Appointment) (model.Appointment, error) {
+	sqlStatement := `UPDATE appointment SET petid=$2, serviceproviderid=$3, servicetype=$4, servicetime=$5 WHERE Id=$1`
+
+	err := validator.ValidateAppointment(app)
+
+	if err != nil {
+		return model.Appointment{}, fmt.Errorf("validation error: %w", err)
+	}
+
+	_, err = appointment_connection.Exec(sqlStatement, id, app.PetId, app.ServiceProviderId, app.ServiceType, app.ServiceTime)
+	if err != nil {
+		return model.Appointment{}, fmt.Errorf("failed to update appointment: %w", err)
+	}
+
+	appResponse := model.Appointment{
+		Id:                id,
+		PetId:             app.PetId,
+		ServiceProviderId: app.ServiceProviderId,
+		ServiceType:       app.ServiceType,
+		ServiceTime:       app.ServiceTime,
+	}
+
+	fmt.Println("successfully update appointment")
+	return appResponse, nil
+}
